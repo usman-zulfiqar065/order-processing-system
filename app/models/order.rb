bab8a9ea1836f524a468621393670c2034cd3d90
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2024-01-09 13:23:44 -0800

    Added required methods and scopes

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,21 @@ class Order < ApplicationRecord
   belongs_to :user
 
   enum :order_type, { buy: 0, sell: 1 }
-  enum :status, { pending: 0, completed: 1, completed: 2 }
+  enum :status, { pending: 0, completed: 1, canceled: 2 }
+
+  scope :completed_buy_orders, -> { completed.buy }
+
+  PROCESS_ORDER_BUY_THRESHOLD_VALUE = 20
+  PROCESS_ORDER_SELL_THRESHOLD_VALUE = 40
+
+  def process_order
+    if (buy? && price < PROCESS_ORDER_BUY_THRESHOLD_VALUE) || (sell? && price > PROCESS_ORDER_SELL_THRESHOLD_VALUE)
+      update(status: 'completed')
+    else
+      update(status: 'canceled')
+    end
+    rescue StandardError => e
+      Rails.logger.error("Error processing order: #{e.message}")
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :orders
+
+  def total_quantity_of_completed_orders
+    orders.completed.sum(:quantity)
+  end
 end
